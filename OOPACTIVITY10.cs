d0c98dbdfdba1850809c8a31298265bffcdbc373
JHAWY30 <jayrhomervidor30@gmail.com>
2025-07-26 03:57:26 +0800

    OOPACTIVITY10.cs

--- OOPACTIVITY10.cs
+++ OOPACTIVITY10.cs
@@ -10,6 +10,31 @@ namespace OOPACTIVITY10
     {
         static void Main(string[] args)
         {
+             //Program to validate user input for name,age and email
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Name required.");
+            return;
+        }
+
+        Console.Write("Enter age: ");
+        if (!int.TryParse(Console.ReadLine(), out int age) || age < 1 || age > 120)
+        {
+            Console.WriteLine("Invalid age.");
+            return;
+        }
+
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+        {
+            Console.WriteLine("Invalid email.");
+            return;
+        }
+
+        Console.WriteLine("All fields are valid.");
         }
     }
 }

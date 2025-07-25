98a15c520308991be6b3951fefb4c508cf51ebe3
JHAWY30 <jayrhomervidor30@gmail.com>
2025-07-26 04:02:04 +0800

    OOPACTIVITY12.cs

--- OOPACTIVITY12.cs
+++ OOPACTIVITY12.cs
@@ -10,6 +10,19 @@ namespace OOPACTIVITY12
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter an integer: ");
+        if (!int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine("Invalid input. Please enter an integer.");
+            return;
+        }
+
+        if (number % 2 == 0)
+            Console.WriteLine("Even number");
+        else
+            Console.WriteLine("Odd number");
+Write to Jay Rhomer Vidor
+
         }
     }
 }
